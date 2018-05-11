efba50cd6142366bdf0e7c52c93104ba644d7fbb
kenji <kenji256@gmail.com>
2018-05-11 10:27:26 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,93 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hButton;
+	static HWND hEdit;
+	static HFONT hFont;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hFont = CreateFontW(-MulDiv(10, 96, 72), 0, 0, 0, FW_NORMAL, 0, 0, 0, SHIFTJIS_CHARSET, 0, 0, 0, 0, L"MS Shell Dlg");
+		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL | ES_NUMBER, 0, 0, 0, 0, hWnd, (HMENU)1000, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("生成"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit, 10, 10, LOWORD(lParam) - 20, 32, TRUE);
+		MoveWindow(hButton, 10, 50, 256, 32, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			srand(GetTickCount());
+			int nSize = GetDlgItemInt(hWnd, 1000, 0, 0);
+			LPBYTE lpByte = (LPBYTE)GlobalAlloc(0, nSize);
+			for (int i = 0; i < nSize; i++)
+			{
+				lpByte[i] = (BYTE)(rand() % 256);
+			}
+			HANDLE hFile = CreateFile(TEXT("rand.bin"), GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+			if (hFile != INVALID_HANDLE_VALUE)
+			{
+				DWORD dwWriteSize;
+				WriteFile(hFile, lpByte, nSize, &dwWriteSize, 0);
+				CloseHandle(hFile);
+			}
+			GlobalFree(lpByte);
+			MessageBox(hWnd, TEXT("出力されました。"), TEXT("確認"), 0);
+		}
+		break;
+	case WM_DESTROY:
+		DeleteObject(hFont);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("指定されたサイズのファイルを作成する。ファイルの中身はランダム。"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
